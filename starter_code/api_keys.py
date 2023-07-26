5c90357ba57ba78415507c698d572ad0fac23a34
Rachael Caldwell <rachael.piper@cms.k12.nc.us>
2023-07-26 10:10:09 -0400

    final upload

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "da7939f10bc281a4400d9048971d0f3d"
+weather_api_key = "946d4a174681bf3b8542e449cdc783a4"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "67150afe96574ad6895eb8b71e1f881c"
